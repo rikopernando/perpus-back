ced98cb21745ecf02c803872bd3bd924fcb1f71b
Riko Pernando <rikopernando96@gmail.com>
2018-09-06 11:55:17 +0700

    refactor redux

--- controllers/AuthorControllers.js
+++ controllers/AuthorControllers.js
@@ -31,6 +31,7 @@ module.exports = {
             .catch((err) => {
               res.status(500).json({
                   message: 'Fail Read Author!',
+                  errors : err
               });
             })
 
@@ -38,6 +39,7 @@ module.exports = {
         .catch((err) => {
           res.status(500).json({
               message: 'Fail Read Author!',
+              errors : err
           });
         })
     },
@@ -89,6 +91,7 @@ module.exports = {
             .catch((err) => {
               res.status(500).json({
                   message: 'Fail Read Author!',
+                  errors : err
               });
             })
 
@@ -96,8 +99,120 @@ module.exports = {
         .catch((err) => {
           res.status(500).json({
               message: 'Fail Read Author!',
+              errors : err
           });
         })
 
+    },
+
+    create : (req,res) => {
+      const { name } = req.body
+      req.assert('name','Nama harus diisi').notEmpty() 
+      const errors = req.validationErrors()
+      if(!errors){
+          models.Author.count({
+            where: {name : name}
+          })
+          .then((total) => {
+              if(total === 0){
+                  models.Author.create({
+                    name : name        
+                  })
+                  .then((author) => {
+                    res.status(200).json({
+                      message : 'Success Create Author',
+                      data : author
+                    })
+                  })
+                  .catch((err) => {
+                    res.status(500).json({
+                      message : 'Fail Create Author',
+                      errors : [{msg:'Fail Create Author'}]
+                    })
+                  })
+              }else{
+                res.status(500).json({
+                  message : 'Fail Create Author',
+                  errors : [{msg:'Author sudah ada'}]
+                })
+              }
+          })
+          .catch((err) => {
+            res.status(500).json({
+               message : 'Fail Create Author',
+               errors : [{msg:'Fail Create Author'}]
+            })
+          })
+      }else{
+        res.status(500).json({
+          message : 'Fail Create Author',
+          errors : errors
+        })
+      }
+    },
+
+		find : (req,res) => {
+			models.Author.findOne({
+        attributes: ['id','name'],
+        where : { id : req.params.id } 
+      })
+      .then((author) => {
+        res.status(200).json({
+          message : 'Success Find Author',
+          data : author
+        })
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message : 'Fail Find Author',
+          errors : err
+        })
+      })
+		},
+
+    update : (req,res) => {
+      const { name } = req.body
+      models.Author.findOne({ where : { id : req.params.id }}).then((author) => {
+          author.update({ name : name }).then((authors) => {
+            res.status(200).json({
+              message : 'Success Update Author',
+              data : authors
+            })
+          })
+          .catch((err) => {
+            res.status(500).json({
+              message : 'Fail Update Author',
+              errors : err
+            })
+          })
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message : 'Fail Update Author',
+          errors : err
+        })
+      })
+    },
+
+    destroy : (req,res) => {
+      models.Author.findOne({ where : { id : req.params.id }}).then((author) => {
+          author.destroy().then((authors) => {
+            res.status(200).json({
+              message : 'Success Delete Author',
+            })
+          })
+          .catch((err) => {
+            res.status(500).json({
+              message : 'Fail Delete Author',
+              errors : err
+            })
+          })
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message : 'Fail Delete Author',
+          errors : err
+        })
+      })
     }
 }
--- routes/author.js
+++ routes/author.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../middlewares/auth')
-const { index, search } = require('../controllers/AuthorControllers')
+const { index, search, create, find, update, destroy } = require('../controllers/AuthorControllers')
 
 router.get('/', auth, index)
 router.get('/search', auth, search)
+router.post('/create', auth, create)
+router.get('/find/:id', auth, find)
+router.put('/update/:id', auth, update)
+router.delete('/delete/:id', auth, destroy)
 
 module.exports = router;
